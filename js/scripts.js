3df81038fc63a8aa26d4959492a13bbfbfb9dc64
david eliason & tommy bounthma <student@epicodus.com>
2015-04-15 13:17:11 -0700

    ass withdraw function

--- js/scripts.js
+++ js/scripts.js
@@ -9,37 +9,37 @@ var BankAccount = {
 };
 
  var Person = {
-    //  name : "tommy",
-    //  set_name: function(new_name){
-    //      this.name = this.new_name;
-    //  },
-     //
-    //  get_name: function(){
-    //      return this.name;
-    //  }
+
     return_name: function(){
         return this.name;
     }
 
  }
 
-     $(function(){
+ var newUser = Object.create(Person);
+ var newAccount = Object.create(BankAccount);
+
+
+     $(document).ready(function(){
          $("form#create-bank").submit(function(event){
              event.preventDefault();
 
          var newAccountUser = $("input#name").val();
          var newDeposit = parseInt($("input#initial-deposit").val());
 
-         var newUser = Object.create(Person);
+        //  var newUser = Object.create(Person);
          newUser.name = newAccountUser;
 
-         var newAccount = Object.create(BankAccount);
+        //  var newAccount = Object.create(BankAccount);
          newAccount.deposit(newDeposit);
          var balance = newAccount.balance;
 
-         $("#balance").append("<td class='success'>"+balance + "</td>");
+         $(".success").text(balance);
          $("#account-holder").append(newUser.name);
      });
+
+
+
 
      $(function(){
          $("form#money-dep-with").submit(function(event){
@@ -47,12 +47,16 @@ var BankAccount = {
              var inputtedDeposit =  parseInt($("input#deposit-amount").val());
              var inputtedWithdraw = parseInt($("input#withdraw-amount").val());
 
-             var new_balance = newAccount.deposit(inputtedDeposit);
-             console.log(new_balance);
+             newAccount.deposit(inputtedDeposit);
+             var new_balance = newAccount.balance;
+             $(".success").text(new_balance);
+
+              newAccount.withdraw(inputtedWithdraw);
+              var withdrawAccount = newAccount.balance;
+              $(".success").text(new_balance);
 
 
 
-
-         })
-     });
- });
+         });
+       })
+    });
